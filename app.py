29fd1589703ae5d931ecdddca829133214a44af3
tanishkmalviya22 <165447615+Tanishk022@users.noreply.github.com>
2025-10-03 15:44:53 +0530

    final_app.py

--- app.py
+++ app.py
@@ -1,44 +1,109 @@
+# import streamlit as st
+# import pickle
+# import numpy as np
+
+# # Load trained model
+# model = pickle.load(open("regression_model.pkl", "rb"))
+
+# # st.title("Air Quality Prediction App")
+# col1,col2 = st.columns([2,2])
+# with col1:
+#     st.image("https://medicaldialogues.in/wp-content/uploads/2016/11/AIR-POLLUTION.jpg")
+# with col2:
+#     st.title("Air Quality Prediction App")
+
+# # User inputs
+# T = st.number_input("Enter Temperature (°C)", min_value=0.0, step=0.1)
+# TM = st.number_input("Enter Max Temp (°C)", min_value=0.0, step=0.1)
+# Tm = st.number_input("Enter Min Temp (°C)", min_value=0.0, step=0.1)
+# SLP  = st.number_input("Enter Sea Level Pressure", min_value=0.0, step=0.1)
+# H = st.number_input("Enter Humidity (%)", min_value=0.0, step=0.1)
+# VV = st.number_input("Enter Visibility (km)", min_value=0.0, step=0.1)
+# V = st.number_input("Enter Wind Direction", min_value=0.0, step=0.1)
+# VM  = st.number_input("Enter Wind Speed (km/h)", min_value=0.0, step=0.1)
+
+# if st.button("Predict Air Quality"):
+#     features = np.array([[T, TM, Tm, SLP, H, VV, V, VM]])
+#     prediction = model.predict(features)[0]
+
+#     st.subheader(f"Predicted AQI: {round(prediction,2)}")
+
+#     # Easy explanation block
+#     if prediction <= 50:
+#         st.success("✅ Good Air Quality – The air is clean and safe to go outside.")
+#     elif prediction <= 100:
+#         st.info("🙂 Moderate Air Quality – Acceptable air quality, but some pollutants may be present.")
+#     elif prediction <= 200:
+#         st.warning("⚠️ Unhealthy for Sensitive Groups – Children, elderly, and people with health issues should take precautions.")
+#     elif prediction <= 300:
+#         st.error("😷 Very Unhealthy – Everyone may experience health effects, avoid outdoor activities.")
+#     else:
+#         st.error("☠️ Hazardous – Serious health risks, stay indoors and avoid exposure.")
+
+
+
 import streamlit as st
 import pickle
 import numpy as np
 
 # Load trained model
-model = pickle.load(open("regression_model.pkl", "rb"))
-
-# st.title("Air Quality Prediction App")
+model = pickle.load(open("regression_model_2.pkl", "rb"))
+if st.button("Information about AQI Range"):
+        st.image("Screenshot 2025-10-03 113142.png")
 col1,col2 = st.columns([2,2])
 with col1:
     st.image("https://medicaldialogues.in/wp-content/uploads/2016/11/AIR-POLLUTION.jpg")
 with col2:
     st.title("Air Quality Prediction App")
 
+# st.image("Screenshot 2025-10-03 113142.png")
+with st.sidebar:
 # User inputs
-T = st.number_input("Enter Temperature (°C)", min_value=0.0, step=0.1)
-TM = st.number_input("Enter Max Temp (°C)", min_value=0.0, step=0.1)
-Tm = st.number_input("Enter Min Temp (°C)", min_value=0.0, step=0.1)
-SLP  = st.number_input("Enter Sea Level Pressure", min_value=0.0, step=0.1)
-H = st.number_input("Enter Humidity (%)", min_value=0.0, step=0.1)
-VV = st.number_input("Enter Visibility (km)", min_value=0.0, step=0.1)
-V = st.number_input("Enter Wind Direction", min_value=0.0, step=0.1)
-VM  = st.number_input("Enter Wind Speed (km/h)", min_value=0.0, step=0.1)
+    # City = st.text_input("City")fil
+    PM10  = st.number_input("Enter PM10", min_value=0.0,max_value=50.0, step=1.0)
+    PM2_5 = st.number_input("Enter PM2.5", min_value=0.0,max_value=30.0, step=1.0)
+    NO2   = st.number_input("Enter NO2", min_value=0.0,max_value=40.0, step=1.0)
+    O3    = st.number_input("Enter O3", min_value=0.0,max_value=50.0, step=1.0)
+    CO    = st.number_input("Enter CO", min_value=0.0,max_value=1.0, step=1.0)
+    SO2   = st.number_input("Enter SO2", min_value=0.0,max_value=40.0, step=1.0)
+    NH3   = st.number_input("Enter NH3", min_value=0.0,max_value=200.0, step=1.0)
+    
 
 if st.button("Predict Air Quality"):
-    features = np.array([[T, TM, Tm, SLP, H, VV, V, VM]])
-    prediction = model.predict(features)[0]
 
-    st.subheader(f"Predicted AQI: {round(prediction,2)}")
+    features = np.array([[PM10,PM2_5, NO2, O3, CO, SO2,NH3]])
+
+
+    # features = np.array([[T, TM, TT, SLP, H, VV, V, VM]])
+    y_pred = model.predict(features)[0]
+    # prediction = np.clip(prediction, 0, 500)
+
+
+    st.subheader(f"Predicted AQI: {round(y_pred,2)}")
 
     # Easy explanation block
-    if prediction <= 50:
-        st.success("✅ Good Air Quality – The air is clean and safe to go outside.")
-    elif prediction <= 100:
-        st.info("🙂 Moderate Air Quality – Acceptable air quality, but some pollutants may be present.")
-    elif prediction <= 200:
-        st.warning("⚠️ Unhealthy for Sensitive Groups – Children, elderly, and people with health issues should take precautions.")
-    elif prediction <= 300:
-        st.error("😷 Very Unhealthy – Everyone may experience health effects, avoid outdoor activities.")
+    st.subheader("Leval of Concern")
+    if y_pred >= 0 and y_pred <= 51:
+        st.subheader("Good Air Quality")
+        st.success("There are negligible levels of pollutants like particulate matter and ground-level ozone, the air is fresh and crisp, and you can breathe easily without any respiratory discomfort.")
+
+    elif y_pred >= 51 and y_pred <= 100:
+        st.subheader("Moderate Air Quality")
+        st.info("Air quality is acceptable. However, there may be a risk for some people, particularly those who are unusually sensitive to air pollution..")
+        
+    elif y_pred >= 101 and y_pred <= 150:
+        st.subheader("Unhealthy for Sensitive Groups")
+        st.warning("Members of sensitive groups may experience health effects. The general public is less likely to be affected.")
+    elif y_pred >= 151 and y_pred <= 200:
+        st.subheader("Unhealthy")
+        st.warning("Some members of the general public may experience health effects; members of sensitive groups may experience more serious health effects.")
+    elif y_pred >= 201 and y_pred <= 300:
+        st.subheader("Very Unhealthy")
+        st.warning("Health alert –The risk of health effects is increased for everyone.")
     else:
-        st.error("☠️ Hazardous – Serious health risks, stay indoors and avoid exposure.")
+        st.write("Hazardous")
+        st.error("– Serious health risks, stay indoors and avoid exposure.]")
+
 
 
 
